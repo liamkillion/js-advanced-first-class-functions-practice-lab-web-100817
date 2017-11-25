369e87723a0322a5e974aefb85a480d023dd7a5e
Liam Killion <liam.killion@gmail.com>
2017-11-25 16:30:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+function logDriverNames(array){
+  array.forEach(function(ele){
+    console.log(ele['name'])
+  })
+}
+
+function logDriversByHometown(array,location){
+  array.forEach(function(ele){
+    if (ele['hometown']===location){
+      console.log(ele['name'])
+    }
+  })
+}
+
+function driversByRevenue(array){
+  newarray =  array.sort(function(a,b){
+  if (a['revenue']>b['revenue']){
+    return 1
+  }
+  })
+  return newarray
+}
